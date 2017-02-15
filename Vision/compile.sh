4f2a7307b11c9a8412dfab70672ab4769ac6d6a4
William Yount <way1225@live.com>
2017-02-15 18:25:38 -0500

    mods

--- Vision/compile.sh
+++ Vision/compile.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
+mkdir bin
 javac -d bin/ -cp src:/home/pi/wpilib/java/NetworkTables_Mod.jar:/home/pi/wpilib/java/current/lib/WPILib.jar:/home/pi/wpilib/java/current/lib/WPILib-sources.jar:/home/pi/wpilib/java/current/lib/opencv.jar:/home/pi/wpilib/java/current/lib/opencv-sources.jar:/home/pi/wpilib/java/current/lib/cscore.jar:/home/pi/wpilib/java/current/lib/cscore-sources.jar src/com/team3164/vision/Vision.java
 
 cd ./bin
-jar cvfm coproc.jar manifest.txt ./com/team3164/vision/*.class
+jar cvfm coproc.jar ../manifest.txt ./com/team3164/vision/*.class
--- Vision/run.sh
+++ Vision/run.sh
@@ -1,1 +1,3 @@
+#!/bin/sh
+
 java -Djava.library.path="/home/pi/opencv-3.1.0/build/lib/:/home/pi/wpilib/java/lib/" -jar ./bin/coproc.jar 
